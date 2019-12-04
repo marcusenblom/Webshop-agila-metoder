f1699a9cc29e20482195b4bca5c6e829fc0a704d
Marcus Enblom <marcusenblom@Marcuss-MacBook-Pro.local>
2019-12-04 09:17:56 +0100

    Första dagen. Startsidans HTML/CSS

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-window.onload = function() {
+$(document).ready(function() {
 
+  // Funktion som ändrar backgrunden på #header beroende på scroll position //
+  $(document).scroll(function(){
+    if (window.pageYOffset>400) {
+      $("#header").css({"background-color":"white","color":"black"}).addClass("borderShadow").removeClass("borderWhite");
+    }
+    else {
+      $("#header").css({"background-color":"transparent","color":"white"}).removeClass("borderShadow").addClass("borderWhite");
+    }
+  });
 
+  // Funktion som skapar upp produkter på förstasidan //
 
+  for (var i = 1; i < 9; i++) {
 
+    let productContainer = $("<div>").addClass("productContainer").appendTo($(".flex-container"));
 
-  let listOfAllProducts = [];
+    let image = $("<img>").attr("src", "images/jacket" +i+".jpg").addClass("productImage").appendTo(productContainer);
 
-  // Class which will create new products (object) //
-  function Product (title, price, src){
+    let productTitleContainer = $("<div>").addClass("productTitleContainer").appendTo(productContainer);
 
-    this.title = title;
-    this.price = price;
-    this.src = src;
+    let title = $("<span>").html("Winter jacket").addClass("titleFont").appendTo(productTitleContainer);
+
+    let price = $("<span>").html("1299 SEK").addClass("priceSpan").appendTo(productTitleContainer);
 
   }
 
 
+  // Funktion som skapar upp produkter under sektionen: "recommended" //
+  for (var i = 1; i < 4; i++) {
 
-  for (var i = 1; i < 7; i++) {
-    let newProduct = new Product("Sommarjacka " + i, 899, "images/jacket1.jpg");
-    listOfAllProducts.push(newProduct);
+    let productContainer = $("<div>").addClass("recommendedContainer").appendTo($(".recommended-flex"));
+
+    let image = $("<img>").attr("src", "images/jacket" +i+".jpg").addClass("recommendedImage").appendTo(productContainer);
+
   }
-
-  console.log(listOfAllProducts);
-
-  // for (var i = 0; i < listOfAllProducts.length; i++) {
-  //
-  //   let productContainer = document.createElement("div");
-  //   productContainer.className = "product-container";
-  //   let productImageContainer = document.createElement("div");
-  //   productImageContainer.className = "productImageContainer";
-  //   let image = document.createElement("img");
-  //   image.className = "productImage";
-  //   let productTitleContainer = document.createElement("div");
-  //
-  //   image.src = listOfAllProducts[i].src;
-  //   productImageContainer.appendChild(image);
-  //   productTitleContainer.innerHTML = listOfAllProducts[i].title;
-  //
-  //   productContainer.appendChild(productImageContainer);
-  //   productContainer.appendChild(productTitleContainer);
-  //
-  //
-  //   $("#productSection").append(productContainer);
-  //
-  // }
-
-
-}
+});
