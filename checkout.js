c69c17be6b81cbc4456cbc7c652415fa3fe3fd37
Helena Lindahl <helena.l.lindahl@gmail.com>
2019-12-08 11:51:13 +0100

    checkbox function

--- checkout.js
+++ checkout.js
@@ -1,1 +1,5 @@
-
+var checkBoxes = $("checkout_checkbox");
+checkBoxes.change(function () {
+$("#checkout_button").prop('disabled', checkBoxes.filter(':checked').length < 1);
+});
+checkBoxes.change();
