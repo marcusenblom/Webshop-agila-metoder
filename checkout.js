631a2c54255f524ae3d47f85fef24740ac5ba1fb
Helena Lindahl <helena.l.lindahl@gmail.com>
2019-12-08 12:23:00 +0100

    test

--- checkout.js
+++ checkout.js
@@ -2,14 +2,21 @@ $(document).ready(function() {
 
 })
 
-let checkoutButton = $("#checkout_button");
-let checkBoxes = $("#checkout_checkbox");
-checkoutButton.addEventListener("click", function () {
-    $("#checkout_button").prop('disabled', checkBoxes.filter(':checked').length < 1);
-});
-checkBoxes.change();  
+// let checkoutButton = $("#checkout_button");
+// let checkBoxes = $("#checkout_checkbox");
+// checkoutButton.addEventListener("click", function () {
+//     $("#checkout_button").prop('disabled', checkBoxes.filter(':checked').length < 1);
+// });
+// checkBoxes.change();  
     
-
+$("#checkout_checkbox").on("change", function(e){
+    if($("#checkout_checkbox").attr("checked")){
+      $("#checkout_button").button("enable");
+    } else {
+      $(".submit").button("disable");
+    }
+    
+  });
 
 
 
