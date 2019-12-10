7d5deea44c3f5f8cc816a0457b3b2fc7864d74f4
Helena Lindahl <helena.l.lindahl@gmail.com>
2019-12-10 13:40:39 +0100

    test

--- checkout.js
+++ checkout.js
@@ -1,29 +1,53 @@
 $(document).ready(function() {
+    $("#checkout_button").click(function() {
+        $("#form").submit();
+    });
 
-})
+ });
 
-// let checkoutButton = $("#checkout_button");
-// let checkBoxes = $("#checkout_checkbox");
-// checkoutButton.addEventListener("click", function () {
-//     $("#checkout_button").prop('disabled', checkBoxes.filter(':checked').length < 1);
-// });
-// checkBoxes.change();  
-    
-// $("#checkout_checkbox").on("change", function(){
-//     if($("#checkout_checkbox").attr("checked")){
-//       $("#checkout_button").button("enable");
-//     } else {
-//       $("#checkout_button").button("disable");
-//     }
-    
-//   });
+// Wait for the DOM to be ready
+$(function() {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='checkout_registration']").validate({
+      // Specify validation rules
+      rules: {
+        // The key name on the left side is the name attribute
+        // of an input field. Validation rules are defined
+        // on the right side
+        firstname: "required",
+        lastname: "required",
+        phone: "required",
+        street: "required",
+        zipcode: "required",
+        city: "required",
+        email: {
+          required: true,
+          // Specify that email should be validated
+          // by the built-in "email" rule
+          email: true
+        },
 
-$("payment_option").on('click', function() { var checked = $("payment_option").prop('checked');
-console.log(checked);
-   if(checked) { $("#checkout_button").show();  } 
-   else { $("#checkout_button").hide(); }
-                                                 });
+      },
+      // Specify validation error messages
+      messages: {
+        firstname: "Fyll i ditt förnamn",
+        lastname: "Fyll i ditt efternamn",
+        phone: "Fyll i ditt telefonnummer",
+        street: "Fyll i din gatuadress",
+        zipcode: "Fyll i ditt postnummer",
+        city: "Fyll i din ort",
+        email: "Fyll i din emailadress"
+      },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
 
 
 
 
+  
