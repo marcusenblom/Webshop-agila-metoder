f5ed0b653559bf67a7151948e00428adb75d679e
Marcus Enblom <marcusenblom@Marcuss-MBP.mediegruppen.se>
2019-12-12 10:00:55 +0100

    hgb

--- detaljsida.js
+++ detaljsida.js
@@ -1,11 +1,74 @@
 $(document).ready(function() {
 
 
-    let cart = [];
-    let cart2 = [];
-    localStorage.setItem("cart", JSON.stringify(cart));
+  detailproduct = JSON.parse(localStorage.getItem("products-1")) || {};
+
+  $("#h3").html(detailproduct.title)
+  $("#detail-description").html(detailproduct.description)
+  $("#spaan").html(detailproduct.price)
+  imgSrc3 = detailproduct.src;
+  let detailImg = $("<img>").attr("src", imgSrc3);
+  $("#id-detail-innerbox-Img").append(detailImg);
+
+  $("#detailButton").on("click", function() {
+
+    let cart = [detailproduct, detailproduct];
+
+    let list = [];
+
+    if (!localStorage.getItem("cart")) {
+      let currentCartItems = JSON.parse(localStorage.getItem("cart")) || [];
+      console.log(currentCartItems);
+    }
+    // let currentCartItems = localStorage.getItem("cart");
+    // console.log(currentCartItems);
+    //
+    // list.push(currentCartItems);
+    // list.push(JSON.stringify(detailproduct));
+    //
+    // localStorage.setItem("cart", list);
+    //
+    // console.log(localStorage.getItem("cart"));
 
 
+
+    // localStorage.setItem("cart", JSON.stringify(detailproduct));
+    //
+    // localStorage.setItem("cart", JSON.stringify(cart));
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let cart = [];
+    // let cart2 = [];
+    // localStorage.setItem("cart", JSON.stringify(cart));
+    //
+    //
     $(".menu-toggle").click(function() {
         if ($(".menu").hasClass("menu-hide")) {
             $(".menu").removeClass("menu-hide");
@@ -16,55 +79,55 @@ $(document).ready(function() {
             $(".menu").removeClass("menu-show");
         }
     });
-
-
-    detailproduct = JSON.parse(localStorage.getItem("products-1")) || {};
-
-    $("#h3").html(detailproduct.title)
-    $("#detail-description").html(detailproduct.description)
-    $("#spaan").html(detailproduct.price)
-    imgSrc3 = detailproduct.src;
-    let detailImg = $("<img>").attr("src", imgSrc3);
-    $("#id-detail-innerbox-Img").append(detailImg)
-
-    $("#detailButton").on("click", function() {
-        let hej = cart.push(detailproduct)
-        let h = 1;
-        cart2.push(h)
-
-        ränkare()
-
-        localStorage.setItem("cart", JSON.stringify(cart));
-        console.log("skicka vidare", hej)
-    });
-
-
-
-    let i = 0;
-
-
-    function ränkare() {
-
-        for (i; i < cart2.length; i++) {
-
-
-        }
-        $(".badge-info").html(i)
-
-
-
-
-    }
-
-    $("#buttonShoppingCart").on("click", function() {
-        window.open("checkout.html")
-        window.close("detaljsida.html")
-    })
-
-
-
-
-
+    //
+    //
+    // detailproduct = JSON.parse(localStorage.getItem("products-1")) || {};
+    //
+    // $("#h3").html(detailproduct.title)
+    // $("#detail-description").html(detailproduct.description)
+    // $("#spaan").html(detailproduct.price)
+    // imgSrc3 = detailproduct.src;
+    // let detailImg = $("<img>").attr("src", imgSrc3);
+    // $("#id-detail-innerbox-Img").append(detailImg)
+    //
+    // $("#detailButton").on("click", function() {
+    //     let hej = cart.push(detailproduct)
+    //     let h = 1;
+    //     cart2.push(h)
+    //
+    //     ränkare()
+    //
+    //     localStorage.setItem("cart", JSON.stringify(cart));
+    //     console.log("skicka vidare", hej)
+    // });
+    //
+    //
+    //
+    // let i = 0;
+    //
+    //
+    // function ränkare() {
+    //
+    //     for (i; i < cart2.length; i++) {
+    //
+    //
+    //     }
+    //     $(".badge-info").html(i)
+    //
+    //
+    //
+    //
+    // }
+    //
+    // $("#buttonShoppingCart").on("click", function() {
+    //     window.open("checkout.html")
+    //     window.close("detaljsida.html")
+    // })
+    //
+    //
+    //
+    //
+    //
 
 
 });
