fe17ee28b098dd68efd73ebc6c91a49378b3aced
Marcus Enblom <marcusenblom@Marcuss-MBP.mediegruppen.se>
2019-12-12 13:54:22 +0100

    Merge branch 'master' of https://github.com/3nblom/Webshop---agila-metoder

--- detaljsida.js
+++ detaljsida.js
@@ -1,53 +1,53 @@
 $(document).ready(function() {
 
-  $(".menu-toggle").click(function() {
-    if ($(".menu").hasClass("menu-hide")) {
-      $(".menu").removeClass("menu-hide");
-      $(".menu").addClass("menu-show");
+    $(".menu-toggle").click(function() {
+        if ($(".menu").hasClass("menu-hide")) {
+            $(".menu").removeClass("menu-hide");
+            $(".menu").addClass("menu-show");
 
-    } else {
-      $(".menu").addClass("menu-hide");
-      $(".menu").removeClass("menu-show");
-    }
-  });
-  $(".filter-button").on("click", function() {
-    $(".menu").addClass("menu-hide");
-    $(".menu").removeClass("menu-show");
+        } else {
+            $(".menu").addClass("menu-hide");
+            $(".menu").removeClass("menu-show");
+        }
+    });
+    $(".filter-button").on("click", function() {
+        $(".menu").addClass("menu-hide");
+        $(".menu").removeClass("menu-show");
 
-    let newFilter = this.id;
+        let newFilter = this.id;
 
-    localStorage.setItem("filter", newFilter);
+        localStorage.setItem("filter", newFilter);
 
-    window.open("index.html", "_self");
-  });
+        window.open("index.html", "_self");
+    });
 
-  $(".home").on("click", function(){
-    localStorage.setItem("filter", 0);
-    window.open("index.html", "_self");
-  });
+    $(".home").on("click", function() {
+        localStorage.setItem("filter", 0);
+        window.open("index.html", "_self");
+    });
 
-  function counter() {
-    let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
-    let cartLength = currentCartItems.length;
-    console.log(cartLength);
+    function counter() {
+        let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
+        let cartLength = currentCartItems.length;
+        console.log(cartLength);
 
-    $(".badge-info").html(cartLength);
-  };
-  counter();
-
-
-  detailProduct = JSON.parse(localStorage.getItem("products-1")) || {};
-
-  $("#h3").html(detailProduct.title)
-  $("#detail-description").html(detailProduct.description)
-  $("#spaan").html(detailProduct.price)
-  imgSrc3 = detailProduct.src;
-  let detailImg = $("<img>").attr("src", imgSrc3);
-  $("#id-detail-innerbox-Img").append(detailImg);
+        $(".badge-info").html(cartLength);
+    };
+    counter();
 
 
-  $("#detailButton").on("click", function() {
-    let newArray = [];
+    detailProduct = JSON.parse(localStorage.getItem("products-1")) || {};
+
+    $("#h3").html(detailProduct.title)
+    $("#detail-description").html(detailProduct.description)
+    $("#spaan").html(detailProduct.price)
+    imgSrc3 = detailProduct.src;
+    let detailImg = $("<img>").attr("src", imgSrc3);
+    $("#id-detail-innerbox-Img").append(detailImg);
+
+
+    $("#detailButton").on("click", function() {
+        let newArray = [];
 
     if (localStorage.getItem("cart")) {
       let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
@@ -59,9 +59,7 @@ $(document).ready(function() {
     newArray.push(detailProduct);
     localStorage.setItem("cart", JSON.stringify(newArray));
 
+        $(".badge-info").html(cartLength);
+    });
     counter();
-  });
-
-
-
 });
