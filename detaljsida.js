c5945f0d17ddd605993f1594ad3ee452af4b0915
Marcus Enblom <marcusenblom@Marcuss-MBP.mediegruppen.se>
2019-12-12 13:57:49 +0100

    quick fix

--- detaljsida.js
+++ detaljsida.js
@@ -1,55 +1,54 @@
 $(document).ready(function() {
 
-    $(".menu-toggle").click(function() {
-        if ($(".menu").hasClass("menu-hide")) {
-            $(".menu").removeClass("menu-hide");
-            $(".menu").addClass("menu-show");
+  $(".menu-toggle").click(function() {
+    if ($(".menu").hasClass("menu-hide")) {
+      $(".menu").removeClass("menu-hide");
+      $(".menu").addClass("menu-show");
 
-        } else {
-            $(".menu").addClass("menu-hide");
-            $(".menu").removeClass("menu-show");
-        }
-    });
-    $(".filter-button").on("click", function() {
-        $(".menu").addClass("menu-hide");
-        $(".menu").removeClass("menu-show");
+    } else {
+      $(".menu").addClass("menu-hide");
+      $(".menu").removeClass("menu-show");
+    }
+  });
+  $(".filter-button").on("click", function() {
+    $(".menu").addClass("menu-hide");
+    $(".menu").removeClass("menu-show");
 
-        let newFilter = this.id;
+    let newFilter = this.id;
 
-        localStorage.setItem("filter", newFilter);
+    localStorage.setItem("filter", newFilter);
 
-        window.open("index.html", "_self");
-    });
+    window.open("index.html", "_self");
+  });
 
-    $(".home").on("click", function() {
-        localStorage.setItem("filter", 0);
-        window.open("index.html", "_self");
-    });
+  $(".home").on("click", function() {
+    localStorage.setItem("filter", 0);
+    window.open("index.html", "_self");
+  });
 
-    function counter() {
-        let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
-        let cartLength = currentCartItems.length;
-        console.log(cartLength);
+  function counter() {
+    let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
+    let cartLength = currentCartItems.length;
+    console.log(cartLength);
 
-        $(".badge-info").html(cartLength);
-    };
-    counter();
-
-
-    detailProduct = JSON.parse(localStorage.getItem("products-1")) || {};
-
-    $("#h3").html(detailProduct.title)
-    $("#detail-description").html(detailProduct.description)
-    $("#spaan").html(detailProduct.price)
-    imgSrc3 = detailProduct.src;
-    let detailImg = $("<img>").attr("src", imgSrc3);
-    $("#id-detail-innerbox-Img").append(detailImg);
+    $(".badge-info").html(cartLength);
+  };
+  counter();
 
 
-    $("#detailButton").on("click", function() {
-        let newArray = [];
+  detailProduct = JSON.parse(localStorage.getItem("products-1")) || {};
 
-<<<<<<< HEAD
+  $("#h3").html(detailProduct.title)
+  $("#detail-description").html(detailProduct.description)
+  $("#spaan").html(detailProduct.price)
+  imgSrc3 = detailProduct.src;
+  let detailImg = $("<img>").attr("src", imgSrc3);
+  $("#id-detail-innerbox-Img").append(detailImg);
+
+
+  $("#detailButton").on("click", function() {
+    let newArray = [];
+
     if (localStorage.getItem("cart")) {
       let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
 
@@ -59,23 +58,9 @@ $(document).ready(function() {
     }
     newArray.push(detailProduct);
     localStorage.setItem("cart", JSON.stringify(newArray));
-=======
-        if (localStorage.getItem("cart")) {
-            let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
-            console.log(currentCartItems);
-            for (var i = 0; i < currentCartItems.length; i++) {
-                newArray.push(currentCartItems[i]);
-            }
-        }
-        newArray.push(detailProduct);
-        localStorage.setItem("cart", JSON.stringify(newArray));
->>>>>>> f1c71ecb023da75c2f68f0ddd1d5d05e64413f1e
 
-        $(".badge-info").html(cartLength);
-    });
-    counter();
-<<<<<<< HEAD
+
+    $(".badge-info").html(newArray.length);
+  });
+  counter();
 });
-=======
-});
->>>>>>> f1c71ecb023da75c2f68f0ddd1d5d05e64413f1e
