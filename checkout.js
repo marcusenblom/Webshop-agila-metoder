fa9d0cd6db070bfd87e12261fea0e21a473d09f9
mariaekmaan1991 <mariaekmaan@hotmail.com>
2019-12-16 12:03:48 +0100

    hej

--- checkout.js
+++ checkout.js
@@ -1,55 +1,5 @@
 $(document).ready(function() {
 
-    $("#checkout_button").click(function() {
-        $("#form").submit();
-    });
-
-
-    /*
-
-        // Wait for the DOM to be ready
-        $(function() {
-            // Initialize form validation on the registration form.
-            // It has the name attribute "registration"
-            $("form[name='checkout_registration']").validate({
-                // Specify validation rules
-                rules: {
-                    // The key name on the left side is the name attribute
-                    // of an input field. Validation rules are defined
-                    // on the right side
-                    firstname: "required",
-                    lastname: "required",
-                    phone: "required",
-                    street: "required",
-                    zipcode: "required",
-                    city: "required",
-                    email: {
-                        required: true,
-                        // Specify that email should be validated
-                        // by the built-in "email" rule
-                        email: true
-                    },
-
-                },
-                // Specify validation error messages
-                messages: {
-                    firstname: "Fyll i ditt förnamn",
-                    lastname: "Fyll i ditt efternamn",
-                    phone: "Fyll i ditt telefonnummer",
-                    street: "Fyll i din gatuadress",
-                    zipcode: "Fyll i ditt postnummer",
-                    city: "Fyll i din ort",
-                    email: "Fyll i din emailadress"
-                },
-                // Make sure the form is submitted to the destination defined
-                // in the "action" attribute of the form when valid
-                submitHandler: function(form) {
-                    form.submit();
-                }
-            });
-        });
-
-*/
     $(".menu-toggle").click(function() {
         if ($(".menu").hasClass("menu-hide")) {
             $(".menu").removeClass("menu-hide");
@@ -93,10 +43,10 @@ $(document).ready(function() {
     for (let p = 0; p < cart.length; p++) {
         console.log(cart[p]);
 
-        let Tagdiv = $("<div>").append("<h4>").html(cart[p].title)
+        let Tagdiv = $("<div>").addClass("divClass").append("<h3>").html(cart[p].title)
         let spanPrice = $("<span>").html(cart[p].price)
         let spanSize = $("<span>").html(cart[p].size)
-        items_div.append(Tagdiv).addClass("divClass")
+        items_div.append(Tagdiv)
         Tagdiv.append(spanPrice)
         Tagdiv.append(spanSize)
     };
