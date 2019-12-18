418ea553dccece3476ad7b5a4453433d48cd31f8
Marcus Enblom <marcusenblom@Marcuss-MBP.mediegruppen.se>
2019-12-18 09:46:07 +0100

    fg

--- checkout.js
+++ checkout.js
@@ -1,64 +1,84 @@
 $(document).ready(function() {
 
-    // Öppnar upp/stänger menyn (accordion)
-    $(".menu-toggle").click(function() {
-        if ($(".menu").hasClass("menu-hide")) {
-            $(".menu").removeClass("menu-hide");
-            $(".menu").addClass("menu-show");
-            $(".menu-toggle-container").css("border-bottom", "2px solid black");
+  // Öppnar upp/stänger menyn (accordion)
+  $(".menu-toggle").click(function() {
+    if ($(".menu").hasClass("menu-hide")) {
+      $(".menu").removeClass("menu-hide");
+      $(".menu").addClass("menu-show");
+      $(".menu-toggle-container").css("border-bottom", "2px solid black");
+      $(".dark").css("opacity", "0.15");
 
-        } else {
-            $(".menu").addClass("menu-hide");
-            $(".menu").removeClass("menu-show");
-            $(".menu-toggle-container").css("border-bottom", "0");
-        }
-    });
-    // Stänger menyn samt skickar filter till localStorage
-    $(".filter-button").on("click", function() {
-        $(".menu").addClass("menu-hide");
-        $(".menu").removeClass("menu-show");
-        $(".menu-toggle-container").css("border-bottom", "0");
+    } else {
+      $(".menu").addClass("menu-hide");
+      $(".menu").removeClass("menu-show");
+      $(".menu-toggle-container").css("border-bottom", "0");
+      $(".dark").css("opacity", "0");
+    }
+  });
+  // Stänger menyn samt skickar filter till localStorage
+  $(".filter-button").on("click", function() {
+    $(".menu").addClass("menu-hide");
+    $(".menu").removeClass("menu-show");
+    $(".menu-toggle-container").css("border-bottom", "0");
 
-        let newFilter = this.id;
-        localStorage.setItem("filter", newFilter);
-        window.open("index.html", "_self");
-    });
-    // Skickar användaren till index-html och sätter filter till 0
-    $(".home").on("click", function() {
-        localStorage.setItem("filter", 0);
-        window.open("index.html", "_self");
-    });
+    let newFilter = this.id;
+    localStorage.setItem("filter", newFilter);
+    window.open("index.html", "_self");
+  });
+  // Skickar användaren till index-html och sätter filter till 0
+  $(".home").on("click", function() {
+    localStorage.setItem("filter", 0);
+    window.open("index.html", "_self");
+  });
 
-    // Visar antal produkter i varukorgen (badge)
-    function counter() {
-        let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
+  // Öppnar och stänger favorites
+  $(".favorites-toggle").click(function() {
+    if ($(".favorites").hasClass("favorites-hide")) {
+      $(".favorites").removeClass("favorites-hide");
+      $(".favorites").addClass("favorites-show");
+      $(".favorites").css("width", "400px");
+      $(".favorites-toggle").css("border-bottom", "2px solid black");
+      $(".dark").css("opacity", "0.15");
 
-        let totalAmount = 0;
-        for (var i = 0; i < currentCartItems.length; i++) {
-            totalAmount = totalAmount + currentCartItems[i].quantity;
-        }
-        console.log(totalAmount);
-        if (totalAmount > 0) {
-            $(".badge").css("visibility", "visible");
-        }
+    } else {
+      $(".favorites").addClass("favorites-hide");
+      $(".favorites").removeClass("favorites-show");
+      $(".favorites").css("width", "0");
+      $(".favorites-toggle").css("border-bottom", "0");
+      $(".dark").css("opacity", "0");
+    }
+  });
 
-        $(".badge").html(totalAmount);
-    };
-    counter();
-    // Hämtar cart från localStorage
-    cart = JSON.parse(localStorage.getItem("cart")) || [];
+  // Visar antal produkter i varukorgen (badge)
+  function counter() {
+    let currentCartItems = JSON.parse(localStorage.getItem("cart")) || {};
 
-    let items_div = $("#checkout_container_items");
+    let totalAmount = 0;
+    for (var i = 0; i < currentCartItems.length; i++) {
+      totalAmount = totalAmount + currentCartItems[i].quantity;
+    }
+    console.log(totalAmount);
+    if (totalAmount > 0) {
+      $(".badge").css("visibility", "visible");
+    }
 
-    for (let p = 0; p < cart.length; p++) {
-        console.log(cart[p]);
+    $(".badge").html(totalAmount);
+  };
+  counter();
+  // Hämtar cart från localStorage
+  cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-        let tagDiv = $("<div>").append("<h4>").html(cart[p].title)
-        let spanPrice = $("<span>").html(cart[p].price)
-        let spanSize = $("<span>").html(cart[p].size)
-        items_div.append(tagDiv).addClass("divClass")
-        tagDiv.append(spanPrice)
-        tagDiv.append(spanSize)
-    };
+  let items_div = $("#checkout_container_items");
+
+  for (let p = 0; p < cart.length; p++) {
+    console.log(cart[p]);
+
+    let tagDiv = $("<div>").append("<h4>").html(cart[p].title)
+    let spanPrice = $("<span>").html(cart[p].price)
+    let spanSize = $("<span>").html(cart[p].size)
+    items_div.append(tagDiv).addClass("divClass")
+    tagDiv.append(spanPrice)
+    tagDiv.append(spanSize)
+  };
 
 });
