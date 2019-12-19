eaccfc89bd4fcbf77874a37d14802080b8100e9e
mariaekmaan1991 <mariaekmaan@hotmail.com>
2019-12-19 11:45:33 +0100

    visar total summman

--- checkout.js
+++ checkout.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //hej
   // Öppnar upp/stänger menyn (accordion)
-  $(".menu-toggle").click(function() {
+
+  $(".menu-toggle").click(function () {
     if ($(".menu").hasClass("menu-hide")) {
       $(".menu").removeClass("menu-hide");
       $(".menu").addClass("menu-show");
@@ -16,7 +17,7 @@ $(document).ready(function() {
     }
   });
   // Stänger menyn samt skickar filter till localStorage
-  $(".filter-button").on("click", function() {
+  $(".filter-button").on("click", function () {
     $(".menu").addClass("menu-hide");
     $(".menu").removeClass("menu-show");
     $(".menu-toggle-container").css("border-bottom", "0");
@@ -26,13 +27,13 @@ $(document).ready(function() {
     window.open("index.html", "_self");
   });
   // Skickar användaren till index-html och sätter filter till 0
-  $(".home").on("click", function() {
+  $(".home").on("click", function () {
     localStorage.setItem("filter", 0);
     window.open("index.html", "_self");
   });
 
   // Öppnar och stänger favorites
-  $(".favorites-toggle").click(function() {
+  $(".favorites-toggle").click(function () {
     if ($(".favorites").hasClass("favorites-hide")) {
       $(".favorites").removeClass("favorites-hide");
       $(".favorites").addClass("favorites-show");
@@ -48,6 +49,32 @@ $(document).ready(function() {
       $(".dark").css("opacity", "0");
     }
   });
+
+  function checkoutSum() {
+    let currentCart = JSON.parse(localStorage.getItem("cart")) || [];
+    let sumQuantity;
+    let sumPrice;
+    let  TotalSumDisplay= $("#checkout_sum")
+    let totalAmount = 0;
+    for (let i = 0; i < currentCart.length; ++i) {
+
+      sumQuantity = +currentCart[i].quantity
+      sumPrice = +currentCart[i].price
+      console.log(sumPrice, sumQuantity)
+      totalAmount = sumPrice * sumQuantity
+      console.log(totalAmount)
+      TotalSumDisplay.html(totalAmount);
+
+    };
+
+    console.log(totalAmount + "test")
+    localStorage.setItem("cart", JSON.stringify(currentCart));
+
+    //lägg till quantity gör en eles if som kontrollera om finns fler quantity  om finns sätt ihop priet
+    displayCart()
+
+  };
+  checkoutSum();
 
   // Visar antal produkter i varukorgen (badge)
   function counter() {
@@ -93,13 +120,12 @@ $(document).ready(function() {
 
   function displayCart() {
     let currentCart = JSON.parse(localStorage.getItem("cart")) || [];
-   
-    
-    let idcheckoutcontaineritems=$("#checkout_container_items")
+
+    let idcheckoutcontaineritems = $("#checkout_container_items")
     $("#checkout_container_items").html(" ");
     for (let i = 0; i < currentCart.length; i++) {
       let productContainer = $("<div>").addClass("mainCartContainer").appendTo($(idcheckoutcontaineritems));
-     
+
       let imageDiv = $("<div>").appendTo(productContainer);
       let imgSrc = currentCart[i].src;
       let image = $("<img>").addClass("cartImage").attr("src", imgSrc).appendTo(imageDiv);
@@ -112,72 +138,58 @@ $(document).ready(function() {
 
       let removeContainer = $("<div>").appendTo(productContainer);
 
-      let buttonDecrease = $("<button>").html("-").appendTo(removeContainer).on("click", function () {buttonDecreaseHandelclick(i)})
-  
-      let buttonIncrease= $("<button>").appendTo(removeContainer).html("+").on("click", function (){ buttonIncreaseHandelclick(i)})
-     
-      let buttonRemoveItems=$("<button>").appendTo(removeContainer).html("ta bort").on("click",function(){ handelclickRemoveItems(i)})
-      
+      let buttonDecrease = $("<button>").html("-").appendTo(removeContainer).on("click", function () {
+        buttonDecreaseHandelclick(i)
+      })
+
+      let buttonIncrease = $("<button>").appendTo(removeContainer).html("+").on("click", function () {
+        buttonIncreaseHandelclick(i)
+      })
+
+      let buttonRemoveItems = $("<button>").appendTo(removeContainer).html("ta bort").on("click", function () {
+        handelclickRemoveItems(i)
+      })
+
       let priceContainer = $("<div>").html(currentCart[i].price + " SEK").appendTo(removeContainer);
 
       let removeImg = $("<img>").addClass("removeImage").attr("src", "images/remove.png").appendTo(removeContainer);
-     
-    }
 
-
-    // function checkoutSum(i){
-    //   let currentCart = JSON.parse(localStorage.getItem("cart")) || [];
-    // let currentItems=0;
-    //   for(let i=0;i<currentCart.length;++i){
-    //      if(this.id==currentCart[i].id && currentItems==currentCart[i].quantity){
-    //       let sumQuantity=sumQuantity+ currentCart[i].quantity;
-    //       let sumPrice=sumPrice+ currentCart[i].price;
-    //       let totalsum=sum2*sum
-    //       $("#checkout_sum").html(totalsum)
-    //       localStorage.setItem("cart", JSON.stringify(currentCart));
-    //      }else{
-    //       let sum=sum+currentCart[i].price
-    //       $("#checkout_sum").html(sum);
-    //       localStorage.setItem("cart", JSON.stringify(currentCart));
-    //      }
-
-    //     //lägg till quantity gör en eles if som kontrollera om finns fler quantity  om finns sätt ihop priet
-    //     console.log(sum,"test")
-    //   }
-    //   displayCart()
-    
-    // }
-  
-     function buttonDecreaseHandelclick(i){
-          console.log(i);
-         let DecreaseItemes=currentCart[i].quantity--;
-          localStorage.setItem("cart", JSON.stringify(currentCart));
-          console.log("öka",DecreaseItemes);
-          // checkoutSum()
-          displayCart()
-        }
-      
-  
-    function handelclickRemoveItems(i){
-        let cartSplice=currentCart.splice(i,1);
-        console.log("remove")
-        localStorage.setItem("cart", JSON.stringify(currentCart));
-        // checkoutSum()
-        displayCart()
-       }
-      
+    };
 
 
 
-function buttonIncreaseHandelclick(i){
- 
-  let IncreaseItemes=currentCart[i].quantity++;
-  console.log("öka",IncreaseItemes);
-  localStorage.setItem("cart", JSON.stringify(currentCart));
-  // checkoutSum()
-  displayCart()
-}
-  }
-    displayCart()
-    // checkoutSum()
+
+
+    function buttonDecreaseHandelclick(i) {
+      console.log(i);
+      let DecreaseItemes = currentCart[i].quantity--;
+      localStorage.setItem("cart", JSON.stringify(currentCart));
+      console.log("öka", DecreaseItemes);
+      checkoutSum()
+      displayCart()
+    };
+
+
+    function handelclickRemoveItems(i) {
+      let cartSplice = currentCart.splice(i, 1);
+      console.log("remove")
+      localStorage.setItem("cart", JSON.stringify(currentCart));
+      checkoutSum()
+      displayCart()
+    };
+
+    function buttonIncreaseHandelclick(i) {
+
+      let IncreaseItemes = currentCart[i].quantity++;
+      console.log("öka", IncreaseItemes);
+      localStorage.setItem("cart", JSON.stringify(currentCart));
+      checkoutSum()
+      displayCart()
+    };
+  };
+
+  displayCart();
+
+
+
 });
